74bd71a07f21d2e0c0ea3c5e1e80f750cb87bc3f
silvercobraa <silver.cobra1n@gmail.com>
2016-11-19 18:23:27 -0300

    Se agregó la 2da consulta

--- consultas.sql
+++ consultas.sql
@@ -1,6 +1,13 @@
 -- Consultas
+-- =========
 
--- Obtener el nombre y apellido de de todos los guitarristas estadounidenses
-select p.nombre, p.apellido
-from Persona as p, Instrumento as i, TocaInstrumento as ti
-where p.dni = ti.dni and p.pais_nacimiento = ti.pais_origen and i.id = ti.instrumento and i.nombre = 'Guitarra eléctrica'
+-- 1) Obtener el nombre y apellido de todos los guitarristas estadounidenses
+	select p.nombre, p.apellido
+	from Persona as p, Instrumento as i, TocaInstrumento as ti
+	where p.dni = ti.dni and p.pais_nacimiento = ti.pais_origen and i.id = ti.instrumento and i.nombre = 'Guitarra eléctrica'
+
+-- 2) Obtener el nombre de todos los artistas que hayan lanzado albumes bajo Roadrunner Records, junto con dichos albumes
+	select ar.nombre_artistico, al.nombre
+	from Artista as ar, Album as al, Lanza as l
+	where ar.id = l.artista and al.id = l.album and al.sello = 'Roadrunner Records'
+
